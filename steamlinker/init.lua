c66ee6ea8d1a384e2f12943bdd8684e9fefb0bf9
Luke <Galtrox@users.noreply.github.com>
2017-12-09 18:38:15 +0000

    added steam profiles (CustomURL & SteamID64)

--- steamlinker/init.lua
+++ steamlinker/init.lua
@@ -1,12 +1,26 @@
 
+
 function onTextMessageEvent(serverConnectionHandlerID, targetMode, toID, fromID, fromName, fromUniqueIdentifier, message, ffIgnored)
 
 		if string.find(string.lower(message), "store.steampowered.com/app/") then
-		
-			ts3.printMessageToCurrentTab("[URL=steam://store/"..string.gsub(string.match(message, '/....../'),"/","",2).."]Open in Steam.[/URL]")
+
+				ts3.printMessageToCurrentTab("[URL=steam://store/"..string.gsub(string.match(message, '/....../'),"/","",2).."]Open in Steam.[/URL]")
 
 		end
-		
+
+		if string.find(string.lower(message), "steamcommunity.com/id/") then
+
+				ts3.printMessageToCurrentTab("[URL=steam://openurl/https://"..message.."]Open in Steam.[/URL]")
+
+		end
+
+		if string.find(string.lower(message), "https://steamcommunity.com/profiles/") then
+
+		  ts3.printMessageToCurrentTab("[URL=steam://openurl/https://steamcommunity.com/profiles/"..string.gsub(string.match(message, '%d+'),"/","",2).."]Open in Steam.[/URL]")
+
+			end
+
 	return
 end
+
 
