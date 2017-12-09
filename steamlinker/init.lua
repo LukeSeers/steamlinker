a77e12dbf9e5e7f104ee4363728eb1d9c104137d
Luke <Galtrox@users.noreply.github.com>
2017-12-09 18:39:02 +0000

    Update init.lua

--- steamlinker/init.lua
+++ steamlinker/init.lua
@@ -4,19 +4,19 @@ function onTextMessageEvent(serverConnectionHandlerID, targetMode, toID, fromID,
 
 		if string.find(string.lower(message), "store.steampowered.com/app/") then
 
-				ts3.printMessageToCurrentTab("[URL=steam://store/"..string.gsub(string.match(message, '/....../'),"/","",2).."]Open in Steam.[/URL]")
+			ts3.printMessageToCurrentTab("[URL=steam://store/"..string.gsub(string.match(message, '/....../'),"/","",2).."]Open in Steam.[/URL]")
 
 		end
 
 		if string.find(string.lower(message), "steamcommunity.com/id/") then
 
-				ts3.printMessageToCurrentTab("[URL=steam://openurl/https://"..message.."]Open in Steam.[/URL]")
+			ts3.printMessageToCurrentTab("[URL=steam://openurl/https://"..message.."]Open in Steam.[/URL]")
 
 		end
 
 		if string.find(string.lower(message), "https://steamcommunity.com/profiles/") then
 
-		  ts3.printMessageToCurrentTab("[URL=steam://openurl/https://steamcommunity.com/profiles/"..string.gsub(string.match(message, '%d+'),"/","",2).."]Open in Steam.[/URL]")
+			ts3.printMessageToCurrentTab("[URL=steam://openurl/https://steamcommunity.com/profiles/"..string.gsub(string.match(message, '%d+'),"/","",2).."]Open in Steam.[/URL]")
 
 			end
 
