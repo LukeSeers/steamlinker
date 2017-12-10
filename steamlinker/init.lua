ff93feacf19de9f0f259de6e08b6c1ed0fc96eb3
Luke <Galtrox@users.noreply.github.com>
2017-12-10 13:54:17 +0000

    Fixed links
    the stupid Teamspeak 3 BBcode was braking the links >:I now everything should work :D

--- steamlinker/init.lua
+++ steamlinker/init.lua
@@ -1,24 +1,22 @@
-
-
 function onTextMessageEvent(serverConnectionHandlerID, targetMode, toID, fromID, fromName, fromUniqueIdentifier, message, ffIgnored)
 
 		if string.find(string.lower(message), "store.steampowered.com/app/") then
 
-			ts3.printMessageToCurrentTab("[URL=steam://store/"..string.gsub(string.match(message, '/....../'),"/","",2).."]Open in Steam.[/URL]")
+				ts3.printMessageToCurrentTab("[URL=steam://store/"..string.gsub(string.match(message, '%d+'),"/","",2).."]Open in Steam.[/URL]")
 
-		end
-
-		if string.find(string.lower(message), "steamcommunity.com/id/") then
-
-			ts3.printMessageToCurrentTab("[URL=steam://openurl/https://"..message.."]Open in Steam.[/URL]")
-
-		end
+				end
 
 		if string.find(string.lower(message), "steamcommunity.com/profiles/") then
 
-			ts3.printMessageToCurrentTab("[URL=steam://openurl/https://steamcommunity.com/profiles/"..string.gsub(string.match(message, '%d+'),"/","",2).."]Open in Steam.[/URL]")
+				ts3.printMessageToCurrentTab("[URL=steam://openurl/https://steamcommunity.com/profiles/"..string.gsub(string.match(message, '%d+'),"/","",2).."]Open in Steam.[/URL]")
 
-			end
+				end
+
+		if string.find(string.lower(message), "steamcommunity.com/id/") then
+
+				ts3.printMessageToCurrentTab("[URL="..string.sub(""..string.gsub(""..message.."", "http://", "steam://openurl/http://").."", 6, -7).."]Open in Steam.[/URL]")
+
+				end
 
 	return
 end
